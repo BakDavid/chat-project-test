371f3b8eaf860849ea6919b03bc27a722794e0ea
Bak David <bak.david96@gmail.com>
2022-09-18 21:58:58 +0300

    Added style for Users section and removed some small parts

--- src/containers/HomePage/User.js
+++ src/containers/HomePage/User.js
@@ -4,20 +4,27 @@ const User = (props) => {
   const { user, onClick } = props;
 
   return (
-    <div onClick={() => onClick(user)} className={classes.displayName}>
-      <div className={classes.displayPic}>
-        <img
-          src="https://avatars.githubusercontent.com/u/44194281?v=4"
-          alt=""
-        />
-      </div>
-      <div className={classes.displayUser}>
-        <span className={classes.displayFullName}>
+    <div onClick={() => onClick(user)} className={classes.container}>
+      <img src="https://avatars.githubusercontent.com/u/44194281?v=4" />
+      <span
+        className={
+          user.isOnline
+            ? classes.personalOnlineStatus
+            : classes.personalOfflineStatus
+        }
+      ></span>
+      <div className={classes.informationSection}>
+        <div className={classes.fullName}>
           {user.firstName} {user.lastName}
-        </span>
-        <span
-          className={user.isOnline ? classes.onlineStatus : classes.onlineStatusOff}
-        ></span>
+        </div>
+        <div className={classes.occupation}>
+          {user.occupation == undefined ? "Web Developer" : user.occupation}
+        </div>
+        {user.isOnline ? (
+          <div className={classes.onlineStatus}>Online</div>
+        ) : (
+          <div className={classes.offlineStatus}>Offline</div>
+        )}
       </div>
     </div>
   );
